687a96f6209e8c64c4a99280989b53b0b4cbca4d
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-13 13:23:09 +0300

    Path param + dict

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
@@ -10,3 +10,28 @@ def january(request):
 
 def february(request):
     return HttpResponse("February hello!")
+
+
+def monthly_challenge(request, month):
+
+    challenges = {
+        "january": "January challenge",
+        "february": "February challenge",
+        "march": "March challenge",
+        "april": "April challenge",
+        "may": "May challenge",
+        "june": "June challenge",
+        "july": "July challenge",
+        "august": "August challenge",
+        "september": "September challenge",
+        "october": "October challenge",
+        "november": "November challenge",
+        "december": "December challenge"
+    }
+
+    challenge = challenges.get(month)
+
+    if challenge:
+        return HttpResponse(challenge)
+    else:
+        return HttpResponseNotFound(f"Month {month} not found!")
