03c9ea50a8eaf50969b0ba8307af724ef2702643
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-13 13:11:51 +0300

    Add february view

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
+def january(request):
     return HttpResponse("This works!")
+
+
+def february(request):
+    return HttpResponse("February hello!")
