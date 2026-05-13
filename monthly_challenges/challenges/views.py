d5614cfbb6fa5ec5e5e7ce281af3e17d56bfe1d5
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-13 14:11:19 +0300

    Named URL & reverse

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -38,7 +39,10 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month!")
 
     redirect_month = months[month - 1]
-    redirect_path = f"/challenges/{redirect_month}"
+
+    # redirect_path = f"/challenges/{redirect_month}"
+
+    redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
 
 
