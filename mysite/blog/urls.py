413f03d770245d0f021b85a4dc7fb7844ef38ce9
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-15 11:26:46 +0300

    Add all posts page

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),
     path('posts/', views.posts, name='posts'),
-    path('posts/<slug:slug>/', views.post, name='post'),
+    path('posts/<slug:slug>/', views.post, name='post-detail-page'),
 ]
