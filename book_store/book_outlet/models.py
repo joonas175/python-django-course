698020763803bc4aa120f0434caf03384d8863af
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-18 10:30:17 +0300

    Add validators

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,12 +1,13 @@
 
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0)])
     publication_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
