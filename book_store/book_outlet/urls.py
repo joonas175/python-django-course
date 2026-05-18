cafa71ac0f236dd3f1641599b124e596cd4c660e
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-18 12:50:49 +0300

    Add templates and URLs

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('book_outlet.urls'))
+    path('', views.index, name='index'),
+    path('books/<int:book_id>/', views.book_detail, name='book-detail')
 ]
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def index(request):
+    all_books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        'all_books': all_books
+    })
+
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'book_outlet/book_detail.html', {
+        'book': book
+    })
