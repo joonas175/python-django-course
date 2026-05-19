534f0c7a1936a9fef8fe09739fdcf74ba4d3db2c
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-19 16:34:24 +0300

    Add separate thank you page and redirect

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+
 
 def review(request: HttpRequest):
     if request.method == 'POST':
         name = request.POST.get('name')
-
-        return render(request, 'reviews/thank_you.html', {
-            'name': name,
-        })
+        print(f'Submitted: {name}')
+        return HttpResponseRedirect(reverse('thank_you'))
 
     return render(request, 'reviews/review.html')
+
+
+def thank_you(request: HttpRequest):
+    return render(request, 'reviews/thank_you.html')
