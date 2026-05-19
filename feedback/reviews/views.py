0f5edd6afc8c386c91c50d23025a22c47727ef99
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-19 16:28:49 +0300

    Add thank you page (POST)

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,14 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
 
-def review(request):
+def review(request: HttpRequest):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        return render(request, 'reviews/thank_you.html', {
+            'name': name,
+        })
+
     return render(request, 'reviews/review.html')
