5de6ebbe85f0490386a9bc72c659f321145c9521
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-20 09:30:18 +0300

    Add textarea and number input

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -4,3 +4,5 @@ from django import forms
 
 class ReviewForm(forms.Form):
     user_name = forms.CharField(label='Your username', max_length=100)
+    review_text = forms.CharField(label='Your review', widget=forms.Textarea)
+    rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5, initial=3)
