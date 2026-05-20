d198acc2da21d6d4a4b90df20a3091feda410ee2
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-20 11:26:32 +0300

    FormView

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -4,29 +4,37 @@ from django.urls import reverse
 from .forms import ReviewForm
 from .models import Review
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 # Create your views here.
 
 
-class ReviewView(View):
+class ReviewView(FormView):
+    template_name = 'reviews/review.html'
+    form_class = ReviewForm
+    success_url = '/thank-you/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
     
-    def get(self, request: HttpRequest):
-        form = ReviewForm()
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
 
-    def post(self, request: HttpRequest):
-        form = ReviewForm(request.POST)
+    # def get(self, request: HttpRequest):
+    #     form = ReviewForm()
+    #     return render(request, 'reviews/review.html', {
+    #         'form': form
+    #     })
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('thank_you'))
-        else:
-            return render(request, 'reviews/review.html', {
-                'form': form
-            })
+    # def post(self, request: HttpRequest):
+    #     form = ReviewForm(request.POST)
+
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect(reverse('thank_you'))
+    #     else:
+    #         return render(request, 'reviews/review.html', {
+    #             'form': form
+    #         })
 
 # def review(request: HttpRequest):
 
