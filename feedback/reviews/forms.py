36a48419a168d9ec516543f82df1e3a83dce720e
Joonas Salojärvi <joonas.salojarvi@anders.com>
2026-05-20 09:42:00 +0300

    Add model for Review

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -4,5 +4,5 @@ from django import forms
 
 class ReviewForm(forms.Form):
     user_name = forms.CharField(label='Your username', max_length=100)
-    review_text = forms.CharField(label='Your review', widget=forms.Textarea)
+    review_text = forms.CharField(label='Your review', widget=forms.Textarea, max_length=500)
     rating = forms.IntegerField(label='Your rating', min_value=1, max_value=5, initial=3)
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -14,6 +15,12 @@ def review(request: HttpRequest):
         if form.is_valid():
             name = form.cleaned_data['user_name']
             print(f'Submitted: {name}')
+            review = Review(
+                user_name=form.cleaned_data['user_name'],
+                review_text=form.cleaned_data['review_text'],
+                rating=form.cleaned_data['rating']
+            )
+            review.save()
             return HttpResponseRedirect(reverse('thank_you'))
         else:
             return render(request, 'reviews/review.html', {
